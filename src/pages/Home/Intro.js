fb31e62d81c79393baf7ff768e19ad3b864383fd
SaimSaeed <79637366+SaimSaeed@users.noreply.github.com>
2023-08-09 01:06:33 +0500

    Intro Changed

--- src/pages/Home/Intro.js
+++ src/pages/Home/Intro.js
@@ -22,16 +22,26 @@ export default  function Intro(props) {
     <>
 <section className='container section1 ' id='intro'>
     <div className='row main-box'>
-    <div className='col-12 text-box text-center'>
+    <div className='col-4 text-box text-center'>
 <h1 className=' main-text'>{text}</h1>
 <h4 className=' second-text'>Full Stack Web Developer</h4>
-<p className=' normal-text'>I am a Full Stack Developer in MERN Stack Techonology. I have been working as a Web Developer from 2 years and I have worked on various projects and gained lots of experience.
-</p>
+
 <Link to="projects" spy={true} smooth={true} offset={-50} duration={500} className='btn button'>See My Work</Link>
     </div>
-    {/* <div className='col-6 img'>
-      <img src={props.profile} alt='main-profile' style={{height:"90%",width:"90%",margin:"0 auto"}}/>
-    </div> */}
+     <div className='col-4 img'>
+      <div className='color'>
+   <img src={props.profile} alt='main-profile' style={{height:"100%",width:"100%",position:"absolute",bottom:"10%"}}/>
+
+      </div>
+
+  
+    
+    </div> 
+
+    <div className='col-4'>
+    <p className=' normal-text ' style={{marginTop:"70%",color:"white"}}>I am a Full Stack Developer in MERN Stack Techonology. I have been working as a Web Developer from 2 years and I have worked on various projects and gained lots of experience.
+</p>
+    </div>
     </div>
 </section>
     </>
