73a3186f6e672c6f733f6c431b0c92f442acca1d
SaimSaeed <79637366+SaimSaeed@users.noreply.github.com>
2023-11-12 12:01:13 +0500

    Portfolio

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -15,91 +15,12 @@ export default  function Skills() {
       
       <div className='row container-fluid'>
 <div className='col-6 col-md-4 col-lg-4 box1  boxes'>
-  <svg viewBox="0 0 36 36" className="circular-chart orange">
-      <path className="circle-bg"
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
-      <path className="circle"
-        strokeDasharray="100, 100"
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
-      <text x="18" y="20.35" className="percentage">100%</text>
-    </svg></div>
-<div className='col-6 col-md-4 col-lg-4 box2 boxes'> <svg viewBox="0 0 36 36" className="circular-chart orange">
-      <path className="circle-bg"
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
-      <path className="circle"
-        strokeDasharray="100, 100"
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
-      <text x="18" y="20.35" className="percentage">100%</text>
-    </svg></div>
-<div className='col-6 col-md-4 col-lg-4  box3 boxes'><svg viewBox="0 0 36 36" className="circular-chart orange">
-      <path className="circle-bg"
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
-      <path className="circle"
-        strokeDasharray="90, 100"
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
-      <text x="18" y="20.35" className="percentage">90%</text>
-    </svg></div>
-<div className='col-6 col-md-4 col-lg-4 box4 boxes'><svg viewBox="0 0 36 36" className="circular-chart orange">
-      <path className="circle-bg"
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
-      <path className="circle"
-        strokeDasharray="100, 100"
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
-      <text x="18" y="20.35" className="percentage">100%</text>
-    </svg></div>
-<div className='col-6 col-md-4 col-lg-4  box5 boxes'><svg viewBox="0 0 36 36" className="circular-chart orange">
-      <path className="circle-bg"
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
-      <path className="circle"
-        strokeDasharray="100, 100"
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
-      <text x="18" y="20.35" className="percentage">100%</text>
-    </svg></div>
+ </div>
+<div className='col-6 col-md-4 col-lg-4 box2 boxes'> </div>
+<div className='col-6 col-md-4 col-lg-4  box3 boxes'></div>
+<div className='col-6 col-md-4 col-lg-4 box4 boxes'></div>
+<div className='col-6 col-md-4 col-lg-4  box5 boxes'></div>
 <div className='col-6 col-md-4 col-lg-4 box6 boxes'>
-<svg viewBox="0 0 36 36" className="circular-chart orange">
-      <path className="circle-bg"
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
-      <path className="circle"
-        strokeDasharray="80, 100"
-        d="M18 2.0845
-          a 15.9155 15.9155 0 0 1 0 31.831
-          a 15.9155 15.9155 0 0 1 0 -31.831"
-      />
-      <text x="18" y="20.35" className="percentage">80%</text>
-    </svg>
 </div>
 
 
