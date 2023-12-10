e79b986488185c482e941f587c0750e082ad6478
SaimSaeed <79637366+SaimSaeed@users.noreply.github.com>
2023-12-10 18:49:57 +0500

    Updated

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,25 +6,25 @@ import {getDocs,collection} from "firebase/firestore"
 export default function Projects() {
 
    
-const [project,setProject] = useState([])
+// const [project,setProject] = useState([])
 
-const portfolioProjectsRef = collection(db, "PortfolioProjects")
-useEffect(() => {
-    const getProjects = async ()=>{
-        try {
-         const data = await getDocs(portfolioProjectsRef)
-            const filteredData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
-            setProject(filteredData)
-            console.log(filteredData)
+// const portfolioProjectsRef = collection(db, "PortfolioProjects")
+// useEffect(() => {
+//     const getProjects = async ()=>{
+//         try {
+//          const data = await getDocs(portfolioProjectsRef)
+//             const filteredData = data.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+//             setProject(filteredData)
+//             console.log(filteredData)
         
-        } catch (error) {
-            console.log(error)
-        }
+//         } catch (error) {
+//             console.log(error)
+//         }
 
-    }
-    getProjects()
+//     }
+//     getProjects()
 
-}, []);
+// }, []);
    
 
 
@@ -38,54 +38,54 @@ useEffect(() => {
 
 
   
-    // const data = [
-    //     {
-    //         id: 1,
-    //         title: "TextUtils",
-    //         description:"TextUtils is a Text Editing App",
-    //         imgUrl:"https://drive.google.com/uc?export=view&id=1tKxPLM3F5HfxyP-UOSBmj_ZVIF7UhIli",
-    //         Url:"https://textutils00.surge.sh/"
-    //     },
-    //     {
-    //         id: 2,
-    //         title: "NewsMonkey",
-    //         description:"NewsMonkey is a News Reading App",
-    //         imgUrl:"https://drive.google.com/uc?export=view&id=1hj18_kfb5V32jaZLoG_jBOCK_xSmjWuJ",
-    //         Url:"https://textutils00.surge.sh/"
+    const data = [
+        {
+            id: 1,
+            title: "TextUtils",
+            description:"TextUtils is a Text Editing App",
+            imgUrl:"https://drive.google.com/uc?export=view&id=1tKxPLM3F5HfxyP-UOSBmj_ZVIF7UhIli",
+            Url:"https://textutils00.surge.sh/"
+        },
+        {
+            id: 2,
+            title: "NewsMonkey",
+            description:"NewsMonkey is a News Reading App",
+            imgUrl:"https://drive.google.com/uc?export=view&id=1hj18_kfb5V32jaZLoG_jBOCK_xSmjWuJ",
+            Url:"https://textutils00.surge.sh/"
     
-    //     },
-    //     {
-    //         id: 3,
-    //         title: "TextUtils",
-    //         description:"TextUtils is a Text Editing App",
-    //         imgUrl:"https://drive.google.com/uc?export=view&id=1tKxPLM3F5HfxyP-UOSBmj_ZVIF7UhIli",
-    //         Url:"https://textutils00.surge.sh/"
-    //     },
-    //     {
-    //         id: 4,
-    //         title: "TextUtils",
-    //         description:"TextUtils is a Text Editing App",
-    //         imgUrl:"https://drive.google.com/uc?export=view&id=1tKxPLM3F5HfxyP-UOSBmj_ZVIF7UhIli",
-    //         Url:"https://textutils00.surge.sh/"
-    //     },
-    //     {
-    //         id: 5,
-    //         title: "TextUtils",
-    //         description:"TextUtils is a Text Editing App",
-    //         imgUrl:"https://drive.google.com/uc?export=view&id=1tKxPLM3F5HfxyP-UOSBmj_ZVIF7UhIli",
-    //         Url:"https://textutils00.surge.sh/"
-    //     },
-    //     {
-    //         id: 6,
-    //         title: "TextUtils",
-    //         description:"TextUtils is a Text Editing App",
-    //         imgUrl:"https://drive.google.com/uc?export=view&id=1tKxPLM3F5HfxyP-UOSBmj_ZVIF7UhIli",
-    //         Url:"https://textutils00.surge.sh/"
-    //     },
+        },
+        {
+            id: 3,
+            title: "TextUtils",
+            description:"TextUtils is a Text Editing App",
+            imgUrl:"https://drive.google.com/uc?export=view&id=1tKxPLM3F5HfxyP-UOSBmj_ZVIF7UhIli",
+            Url:"https://textutils00.surge.sh/"
+        },
+        {
+            id: 4,
+            title: "TextUtils",
+            description:"TextUtils is a Text Editing App",
+            imgUrl:"https://drive.google.com/uc?export=view&id=1tKxPLM3F5HfxyP-UOSBmj_ZVIF7UhIli",
+            Url:"https://textutils00.surge.sh/"
+        },
+        {
+            id: 5,
+            title: "TextUtils",
+            description:"TextUtils is a Text Editing App",
+            imgUrl:"https://drive.google.com/uc?export=view&id=1tKxPLM3F5HfxyP-UOSBmj_ZVIF7UhIli",
+            Url:"https://textutils00.surge.sh/"
+        },
+        {
+            id: 6,
+            title: "TextUtils",
+            description:"TextUtils is a Text Editing App",
+            imgUrl:"https://drive.google.com/uc?export=view&id=1tKxPLM3F5HfxyP-UOSBmj_ZVIF7UhIli",
+            Url:"https://textutils00.surge.sh/"
+        },
     
     
     
-    // ]
+    ]
 
 
    
@@ -101,7 +101,7 @@ useEffect(() => {
 
                     </div>
 {
-                   project.map((element)=>{
+                   data.map((element)=>{
              return       <Cards   key={element.id} title={element.title} description={element.description} img={element.imgUrl}  Url={element.Url}/>
                    }
                    )
